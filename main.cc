fc4df3683d05dfbc7076a54972774fc945465cdf
b26wei <b26wei@student.cs.uwaterloo.ca>
2019-11-24 00:58:51 -0500

    message

--- main.cc
+++
@@ -1,9 +0,0 @@
-#include <string>
-#include <iostream>
-#include "controller.h"
-using namespace std;
-
-int main(){
-    Controller c;
-    c.run(); // runs the controller (Ethan plz make it better <3)
-}
