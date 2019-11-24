44e78325a1857ca73be6893220a251998e304221
eofarrel <eofarrel@student.cs.uwaterloo.ca>
2019-11-24 00:48:05 -0500

    second attempt at leveltest kill

--- level_test.cc
+++
@@ -1,9 +0,0 @@
-#include "level_test.h"
-#include "block_O.h"
-#include <memory>
-
-LevelTest::~LevelTest(){}
-
-std::unique_ptr<Block> LevelTest::spawnBlock(){
-    return std::make_unique<BlockO>();
-}
