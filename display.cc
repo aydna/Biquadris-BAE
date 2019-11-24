375216aea23e888530c31b4276c0311ef3cb6dcb
yz8yang <yz8yang@student.cs.uwaterloo.ca>
2019-11-23 22:35:10 -0500

    game/display v1.0

--- display.cc
+++ display.cc
@@ -1,58 +1,87 @@
 #include "display.h"
 #include <vector>
-#include <memory>
 #include <utility>
+#include <memory>
 #include <iostream>
 
-Display::Display() { //sets the right size for the 2d vector, fills with '-', change it spaces at the end?
-    for (int row = 0; row < boardSize.first; ++row){
+
+Display::Display(Game* game) {
+    my_game = game;
+    
+    p1.boardSize = std::pair<int,int>(18,11);
+    p2.boardSize = std::pair<int,int>(18,11);
+
+    p1.score = 0;
+    p2.score = 0;
+
+    p1.level = 0;
+    p2.level = 0;
+
+    for (int row = 0; row < p1.boardSize.first; row++) {
         std::vector<char> newRow;
-        for (int col = 0; col < boardSize.second; ++col){
-            newRow.emplace_back('-');
+        for (int col = 0; col < p1.boardSize.second; col++) {
+            newRow.emplace_back(".");
         }
-        grid1.emplace_back(newRow);
+        p1.grid.emplace_back(newRow);
     }
-    for (int row = 0; row < boardSize.first; ++row){
-        std::vector<char> newRow;
-        for (int col = 0; col < boardSize.second; ++col){
-            newRow.emplace_back('-');
-        }
-        grid2.emplace_back(newRow);
-    }
+    //for now
+    p2.grid = p1.grid;
 }
 
-void Display::update(const std::vector<std::unique_ptr<Block>> &blocks1, const std::vector<std::unique_ptr<Block>> &blocks2) {
-    // clear board
-    for (int row = 0; row < boardSize.first; ++row){
-        for (int col = 0; col < boardSize.second; ++col){
-            grid1[row][col] = '-';
-            grid2[row][col] = '-';
+void Display::update() {
+
+    // clear board (for now)
+    for (int row = 0; row < p1.boardSize.first; ++row){
+        for (int col = 0; col < p1.boardSize.second; ++col){
+            p1.grid[row][col] = ".";
+            p2.grid[row][col] = ".";
         }
     }
-    // fills with blocks 
-    for (auto const& b : blocks1){
-        for (auto p : b->pixels){
-            grid1[p.first][p.second] = 'O'; //blocks will have a Char class to be added
+
+    //for p1 board
+    Board* b1 = game->getBoard(1);
+    std::vector<unique_ptr<Block>> p1Blocks = b1->getBlocks();
+
+    for (auto &b : p1Blocks) {
+        char curr_color = b.getColor();
+        for (auto p : p1Blocks->pixels) {
+            p1.grid[p.first][p.second] = curr_color;
         }
     }
-    for (auto const& b : blocks2){
-        for (auto p : b->pixels){
-            grid2[p.first][p.second] = 'O';
+
+
+    //for p2 board
+    Board* b2 = game->getBoard(2);
+    std::vector<unique_ptr<Block>> p2Blocks = b2->getBlocks();
+
+    for (auto &b : p2Blocks) {
+        char curr_color = b.getColor();
+        for (auto p : p2Blocks->pixels) {
+            p1.grid[p.first][p.second] = curr_color;
         }
     }
+    
 }
 
 
-std::ostream &operator <<(std::ostream &out , const Display &d){
-    for (int row = 0; row < d.boardSize.first; ++row){ 
-        for (int col = 0; col < d.boardSize.second; ++col){
-            out << d.grid1[row][col];
+std::ostream &operator <<(std::ostream &out, const Display &d) {
+    out << "Level:    " << d.p1.level << "      "; // 6 spaces between the boards
+    out << "Level:    " << d.p2.level << std::endl;
+    out << "Score:    " << d.p1.level << "      ";
+    out << "Score:    " << d.p2.level << std::endl;
+    out << "-----------      -----------";
+
+    for (int row = 0; row < d.p1.boardSize.first; row++) {
+        for (int col = 0; col < d.p1.boardSize.second; col++) {
+            out << d.p1.grid[row][col];
         }
-        out << "     ";
-        for (int col = 0; col < d.boardSize.second; ++col){
-            out << d.grid2[row][col];
+        out << "      ";
+        for (int col = 0; col < d.p2.boardSize.second; col++) {
+            out << d.p2.grid[row][col];
         }
         out << std::endl;
     }
+    
+    //out << "Next:" for later impl
     return out;
 }
--- game.h
+++ game.h
@@ -4,8 +4,35 @@
 #include <utility>
 #include <iostream>
 #include "board.h"
-#include "display.h"
 
+
+class Game {
+    // Private fields
+    std::pair<int,int> boardSize;
+    int currPlayerTurn;
+    std::unique_ptr<Board> b1;
+    std::unique_ptr<Board> b2;
+    public:
+    // Ctor v1.0 (accomodates for multiple players (2 for phase0))
+    Game();
+    int getPlayer(); //gets curr player's turn
+    bool gameOver();
+    Board* getBoard(int player); 
+    void levelUp(int lvl);
+    void drop();
+    void rotateCW(int mult);
+    void rotateCCW(int mult);
+    void moveRight(int mult);
+    void moveLeft(int mult);
+    void moveDown(int mult);
+    void makeSpecial(std::string type, int mult); //special effects
+    void giveLevelBlockSeq(std::string fname);
+    void removeLevelBlockSeq(std::string fname);
+};
+
+
+
+/*
 class Game{
     Display *d;
     // also figure out the 3 reserve rows problem
@@ -21,5 +48,7 @@ class Game{
         void moveRight();
         void drop();
 };
+*/
+
 
 #endif
