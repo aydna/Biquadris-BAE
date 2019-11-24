806eea1c1af7f558e56f7384f39e02122a894231
eofarrel <eofarrel@student.cs.uwaterloo.ca>
2019-11-24 01:42:27 -0500

    finally adding levels 0 and 1 to develop

---
+++ level1.h
@@ -0,0 +1,20 @@
+//level1.h
+#ifndef LEVEL1_H
+#define LEVEL1_H
+
+#include <string>
+#include <memory>
+#include "level.h"
+
+class Block; //might give multiple declaration error
+
+class Level1: public Level{
+    public:
+        Level1(std::string file = "", bool useRandom = true);
+        ~Level1();
+
+        virtual std::unique_ptr<Block> spawnBlock() override;
+};
+
+
+#endif
